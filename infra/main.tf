b7f0017ee0d0ebe0d044229d81bfb31165fb81ea
almcgale-hash <al.mcgale@gmail.com>
2025-12-23 13:30:12 -0500

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-devsecops-lab"
+  location = "canadacentral"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-devsecops-lab"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  os_type  = "Windows"
+  sku_name = "F1" # Free tier if available
+}
+
+resource "azurerm_windows_web_app" "app" {
+  name                = "devsecops-lab-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {}
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
